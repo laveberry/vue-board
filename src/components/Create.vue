3b8938d58968556a494da276784ad6b09e77c4ca
laveberry <laveberry7@gmail.com>
2021-06-25 14:31:14 +0900

    vue.js 게시판만들기

---
+++ src/components/Create.vue
@@ -0,0 +1,38 @@
+// vue 만들고 라우터로 가서 경로설정
+
+<template>
+    <div>
+        <input v-model="writer" placeholder="글쓴이">
+        <input v-model="title" placeholder="제목">
+        <textarea v-model="content" placeholder="내용"></textarea>
+        <button @click="write">작성</button>
+    </div>
+</template>
+<script>
+import data from '@/data'
+
+export default {
+    name: 'Create',
+    data(){
+        return {
+            data: data,
+            writer:"",
+            title:"",
+            content:""
+        }
+    },
+    methods:{
+        write(){
+            //data에 리스트 추가
+            this.data.push({
+                writer : this.writer,
+                title : this.title,
+                content : this.content,
+            })
+            this.$router.push({
+                path : '/'
+            })
+        }
+    }
+}
+</script>
