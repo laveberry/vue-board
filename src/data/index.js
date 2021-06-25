100c05f03dd70bbfe3ddf980e4c89489835f8f60
laveberry <laveberry7@gmail.com>
2021-06-25 14:31:14 +0900

    vue게시판 만들기 1일차
    Signed-off-by: laveberry <laveberry7@gmail.com>

---
+++ src/data/index.js
@@ -0,0 +1,18 @@
+// db대체
+export default [
+    {
+        writer: '아이린',
+        title: '레드벨벳',
+        content: '1'
+    },
+    {
+        writer: '조이',
+        title: '레드벨벳',
+        content: '2'
+    },
+    {
+        writer: '슬기',
+        title: '레드벨벳',
+        content: '3'
+    },
+]
---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Read from '@/components/Read'
+import Create from '@/components/Create'
+import Detail from '@/components/Detail'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Read',
+      component: Read
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: Create
+    },
+    {
+      path: '/detail/:contentid',
+      name: 'Detail',
+      component: Detail
+    },
+  ]
+})
